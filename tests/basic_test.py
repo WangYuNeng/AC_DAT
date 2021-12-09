b8860d37ac05c44a703c0827355ee1532a66ba74
WangYuNeng <ken789911@gmail.com>
2021-12-09 14:47:41 -0800

    Hotfix plot saving path

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
             for trial in tqdm(range(n_round)):
                 mgr.generate_random_ksat_formula(k, n_clauses, n_vars)
                 is_sat = mgr.solve(solver=solver)
-                save_path = os.path.join(var_dir, "{}_{}".format(is_sat, n_round))
+                save_path = os.path.join(var_dir, "{}_{}".format(is_sat, trial))
                 solver.plot_sol(save_path)
